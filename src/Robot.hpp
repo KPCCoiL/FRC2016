50226a4ac7209ef7dbe3498e5d26667b0cca72ff
KPCCoiL <akifumi@Macbook-Pro-of-Chitose.local>
2016-02-21 14:15:48 +0900

    fixed indentation

--- src/Robot.hpp
+++ src/Robot.hpp
@@ -14,42 +14,42 @@ constexpr double sig(double x) {
 
 class Robot: public IterativeRobot {
 
-    Joystick stick; // only joystick
-    LiveWindow* lw;
-    CANTalon leftMotor, rightMotor, arm;
-    std::chrono::time_point<std::chrono::system_clock> startTime;
-    std::size_t func_id;
-    std::array<std::pair<std::function<double(double)>, std::string>, 5> const funcs = {{
-        {[](auto x) { return std::tan(x) / std::tan(1); }, "y=tanx/tan1"},
-        {[](auto x) { return sig(x) * x * x; }, "y=sig(x)x^2"},
-        {[](auto x) { return sig(x) * x * std::tan(x) / std::tan(1); }, "y=sig(x)xtanx/tan1"},
-        {[](auto x) { return x * x * x; }, "y=x^3"},
-        {[](auto x) { return std::pow(x, 5); }, "y=x^5"}
-    }};
+        Joystick stick; // only joystick
+        LiveWindow* lw;
+        CANTalon leftMotor, rightMotor, arm;
+        std::chrono::time_point<std::chrono::system_clock> startTime;
+        std::size_t func_id;
+        std::array<std::pair<std::function<double(double)>, std::string>, 5> const funcs = {{
+            {[](auto x) { return std::tan(x) / std::tan(1); }, "y=tanx/tan1"},
+            {[](auto x) { return sig(x) * x * x; }, "y=sig(x)x^2"},
+            {[](auto x) { return sig(x) * x * std::tan(x) / std::tan(1); }, "y=sig(x)xtanx/tan1"},
+            {[](auto x) { return x * x * x; }, "y=x^3"},
+            {[](auto x) { return std::pow(x, 5); }, "y=x^5"}
+        }};
 
-    enum StickButtons {
-        ButtonA = 1,
-        ButtonB,
-        ButtonX,
-        ButtonY,
-        ButtonL,
-        ButtonR,
-        ButtonBack,
-        ButtonStart,
-        ButtonLeftJoyStick,
-        ButtonRightJoyStick,
-    };
+        enum StickButtons {
+            ButtonA = 1,
+            ButtonB,
+            ButtonX,
+            ButtonY,
+            ButtonL,
+            ButtonR,
+            ButtonBack,
+            ButtonStart,
+            ButtonLeftJoyStick,
+            ButtonRightJoyStick,
+        };
+
+        void AutonomousInit();
+        void AutonomousPeriodic();
+        void TeleopInit();
+        void TeleopPeriodic();
+        void TestPeriodic();
 
     public:
-    Robot();
-    static constexpr double UpdatePeriod = 0.010;
+        Robot();
+        static constexpr double UpdatePeriod = 0.010;
 
-    private:
-    void AutonomousInit();
-    void AutonomousPeriodic();
-    void TeleopInit();
-    void TeleopPeriodic();
-    void TestPeriodic();
 };
 
 #endif //end of include guard
